53ad25e0e2471376624c4aa95c8f1deb3d96a4f0
Nguyễn Tấn Hào <tanhaonguyen2015@gmail.com>
2021-04-25 14:22:09 +0700

    First commit
    - Finish logic coding in project.ino
    - Still have firebase incomplete in flow.json (node-red)

---
+++ project.ino
@@ -0,0 +1,345 @@
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+int melody[] = {
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D5,
+  NOTE_C5, NOTE_C5, NOTE_C5, NOTE_C5, 
+  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5, 
+  NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5,
+  NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5, 
+  NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5, 
+  NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_F4,
+  NOTE_G4, 0, NOTE_G4, NOTE_D5,
+  NOTE_C5, 0, NOTE_AS4, 0,
+  NOTE_A4, 0, NOTE_A4, NOTE_A4,
+  NOTE_C5, 0, NOTE_AS4, NOTE_A4, 
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+  NOTE_G4, 0, NOTE_G4, NOTE_D5,
+  NOTE_C5, 0, NOTE_AS4, 0,
+  NOTE_A4, 0, NOTE_A4, NOTE_A4,
+  NOTE_C5, 0, NOTE_AS4, NOTE_A4, 
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5
+ };
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  };
+//--------------------------------------------------------------------------
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "SugarDadda";
+const char* password = "123456789";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+//---------------------------------------------------
+const int LED = D1;
+const int buzzer = D2;
+const int n = 2;
+const int trig[n] = {D3, D5};
+const int echo[n] = {D4, D6};
+//---------------------------------------------------
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  // '1'/'2': powerOn/Off, '3'/'4': buzzerOn/Off, '5'/'6': lightOn/Off, '7'/'8'/'9': checkPower/Buzzer/Light.
+  switch ((char)payload[0]) {
+    case '1':
+    case '2':
+      digitalWrite(buzzer, LOW);
+      digitalWrite(LED, LOW);
+      break;
+    case '3':
+      //digitalWrite(buzzer, HIGH);
+      for (int thisNote = 0; thisNote < 112; thisNote++) {
+        int noteDuration = 750 / noteDurations[thisNote];
+        tone(buzzer, melody[thisNote], noteDuration);
+    
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        
+        noTone(buzzer);
+      }
+      break;
+    case '4':
+      digitalWrite(buzzer, LOW);
+      break;
+    case '5':
+      digitalWrite(LED, HIGH);
+      break;
+    case '6':
+      digitalWrite(LED, LOW);
+      break;
+//    case '7':
+//    case '8':
+//    case '9':
+  }
+//  if ((char)payload[0] == '1') {
+//    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+//    digitalWrite(LED, HIGH);
+//    // but actually the LED is on; this is because
+//    // it is active low on the ESP-01)
+//  } else {
+//    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+//    digitalWrite(LED, LOW);
+//  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("elcis1210", "hello world");
+      // ... and resubscribe
+      client.subscribe("elcis1210");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(LED, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+//  for (int i = 0; i < n; i++){
+//    pinMode(trig[i], OUTPUT);
+//    pinMode(echo[i],INPUT);
+//  }
+    
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  //------------------------------------------
+  digitalWrite(BUILTIN_LED, HIGH);
+}
+
+float* getDistance(){
+  float duration;
+  float* distanceCm = new float[n];
+  
+  for (int i = 0; i < n; i++){
+    digitalWrite(trig[i], LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig[i], HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig[i], LOW);
+  
+    duration = pulseIn(echo[i], HIGH);
+    Serial.print("Duration:");
+    Serial.println(duration);
+    distanceCm[i] = duration * 0.034 / 2;
+  }
+
+  Serial.println("---------------------");
+  return distanceCm;
+}
+void loop() {
+  //Serial.print("trig:");
+  //Serial.println(trig1);
+  //float a[n] = getDistance();
+  //Serial.println(a);
+  float* re = getDistance();
+  for (int i = 0; i < n; i ++){
+    Serial.print(re[i]);
+    Serial.print(" ");
+  }
+  Serial.println("*********************************");
+  delay(100);
+  
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+  //------------------------------------------------------------
+  
+}
